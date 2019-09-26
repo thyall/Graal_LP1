fc6ac96b24aab94a1d7bb70869a262ca501ba6b0
thyall <thyall96@gmail.com>
2019-09-26 16:38:14 -0300

    overload?

--- Over_equal.cpp
+++ Over_equal.cpp
@@ -6,7 +6,7 @@ bool equal(itr first, itr last, itr first2, Equal eq)
 	while(first != last)// a loço serve para procurar se os ranges diferem
 	{ 
 		// segue mesma logica das funções anteriores
-		if(!eq(*first, *first2))// se a função eq retornar true, se ela for false, ou seja, valores diferentes o if fica valido "True" e a função retorna false
+		if(!eq(*first, *first2))// se a função eq retornar true é negado pelo "!"e não retorna false, se ela for false, ou seja, valores diferentes o if fica valido (True) e a função retorna false
 			return false;
 		else
 		{
@@ -14,17 +14,29 @@ bool equal(itr first, itr last, itr first2, Equal eq)
 			first2++;//percorrendo ambos ranges
 		}
 	}
-	return true;//se o valor da função não for achado no range a função retornar last
+	return true;//se as funções não forem diferentes retorna true
 }
 
+////---------------> tentando entender a logica do outra para diferenciar ambas e ter o overload<-------------
 
-////---------------> tentnado entender a logica do outra para ter o overload<-------------
+template <typename itr, typename Equal>
+bool equal (itr first, itr last, itr first2, itr last2, Equal eq)
+{
+	while(first != last)// a loço serve para procurar se os ranges diferem
+	{ 
+		// segue mesma logica das funções anteriores
+		if(!eq(*first, *first2))// se a função eq retornar true é negado pelo "!"e não retorna false, se ela for false, ou seja, valores diferentes o if fica valido (True) e a função retorna false
+			return false;
+		else
+		{
+			first++;
+			first2++;//percorrendo ambos ranges
+		}
+	}
+	return true;//se as funções não forem diferentes retorna true
+}
 
-/*template <typename itr, typename equal>
-bool equal (itr first, itr last, itr first2, itr last2, equal eq)*/
-
-////---------------> tentnado entender a logica do outra para ter o overload<-------------
-
+////---------------> tentando entender a logica do outra para diferenciar ambas e ter o overload<-------------
 
 //cliente
 bool eq(const int &a, const int &b){return a == b;} 
@@ -32,7 +44,7 @@ bool eq(const int &a, const int &b){return a == b;}
 int main(int argc, char const *argv[])
 {
 	int A[] = {1,2,3,5,4,8,7,54,-5,3};
-	int B[] = {1,2,3,5,4,8,7,54,-5,3};
+	int B[] = {1,2,3,5,4,8,7,54,-5,3};//vetor B igual a A
 	int C[] = {1,3,5,7,9,11,13,15,17,19};
 	
 	auto ret = equal(std::begin(A), std::end(A), std::begin(B), eq);
