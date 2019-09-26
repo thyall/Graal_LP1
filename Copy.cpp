1f0340f4fb87577825c2b69fdec8e39d104099b2
Thyall Dgreville Santiago de Azevedo <thyall@A305-13L.imd.local>
2019-09-26 10:01:46 -0300

    ta hard

--- Copy.cpp
+++ Copy.cpp
@@ -5,12 +5,12 @@ itr copy(itr first, itr last, itr d_first)
 {	
 	while(first != last)
 	{
-		d_first = first;
+		*d_first = *first;
 		first++;
 		d_first++;
 	}
 
-	return d_first-1;
+	return d_first;
 }
 
 
@@ -19,10 +19,9 @@ itr copy(itr first, itr last, itr d_first)
 //cliente
 int main(int argc, char const *argv[])
 {
-	double *retur; 
-
 	//int A[] = {1,2,3,5,4,8,7,54,-5,3};
 	double B[] = {2.5, 3.4, 4.8, 8.8,-3.2, -1.5, 10.89, 7.8, 5.5,45.8}; //recebendo ambos os tipos
+	double C[10];
 
 	for(int i = 0; i < 10; ++i)
 	{
@@ -30,9 +29,14 @@ int main(int argc, char const *argv[])
 	}
 	std::cout << std::endl;
 
-	retur = copy(std::begin(B), std::end(B), std::begin(B));
+	auto last = copy(std::begin(B), std::end(B), std::begin(C));
 
-	std::cout << *retur <<std::endl;
+	for(int i = 0; i < 10; ++i)
+	{
+		std::cout << C[i] << " ";
+	}
+	std::cout << std::endl;
+	                                     
 	
 	return 0;
 }
