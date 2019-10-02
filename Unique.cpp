296925722400f97dfd3c022941409c56ca298c8d
thyall <thyall96@gmail.com>
2019-10-02 09:29:01 -0300

    rever sort e unique

--- Unique.cpp
+++ Unique.cpp
@@ -1,20 +1,52 @@
 #include <iostream>
 
-template <typename itr, typename equal >
-itr unique (itr first, itr last, equal eq)
-{
-	auto val = first;
-	while(first!=last)
-	{	
-		if(!eq(*val == *first))
-		{	
+template<typename Itr, typename Equal>
+    Itr unique(Itr first1, Itr last1, Equal eq)
+    {        
+        Itr inicio = first1;
+        Itr auxiliar = first1 + 1;
 
-		}
-		
+        int size1 = (last1 - 1) - inicio;
+        int array2[size1];
 
-	}
+        Itr ponteiro = first1; 
+        Itr first2 = &array2[0];
+        Itr inicio2 = &array2[0];
+        Itr last2 = &array2[size1];
 
-}
+        while(inicio < last1)
+        {
+            if (auxiliar != inicio && eq(*auxiliar,*inicio))
+            {
+                *last2 = *inicio;
+                last2--;
+                inicio++;
+                auxiliar = first1;
+            }
+            else if(auxiliar == last1 && !eq(*auxiliar,*inicio))
+            {
+                *first2 = *inicio;
+                first2++;
+                inicio++;
+                auxiliar = first1;
+                ponteiro++;                
+            }
+            else
+            {
+                auxiliar++;
+            }
+        }
+        
+        /*for (int i = 0; i <= size1; i++)
+        {
+            *first1 = *inicio2;
+            std::cout << *first1 << " ";
+            first1++;
+            inicio2++;
+        }*/
+
+        return ponteiro;
+    }
 
 
 
@@ -24,9 +56,14 @@ int main()
 {
 	int A[] = {1,1,2,2,3,3,5,5,4,8,7,54,-5,3};
 
-	auto ret = find(std::begin(A), std::end(A),  eq);
+	auto ret = unique(std::begin(A), std::end(A),  eq);
 
 	std::cout << *ret << std::endl;
+
+	for (int i = 0; i < 14; ++i)
+	{
+		std::cout << ret[i];
+	}
 	
 	return 0;
 }
---
+++ sort.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+template<typename Itr, typename Comparison>
+    void sort(Itr first, Itr last, Comparison comp)
+    {
+        for (int i = 0; i < (last - first); ++i)
+        {
+            int aux = i;
+            for (int j = i + 1; j <= (last - first); ++j)
+            {
+                if (comp (*(first + j), *(first+aux)))
+                {
+                    aux = j;
+                }
+            }
+            std::swap(*(first+i),*(first+aux));
+        }
+    }
+
+
+//cliente
+int main()
+{
+	int A[] = {1,2,7,5,4,8,7,54,-5,12};
+
+	std::cout << "Antes:" << std::endl;
+	for(int i = 0; i < 10; ++i)
+	{
+	  std::cout << A[i] << " ";
+	}std::cout << std::endl;
+
+	auto a = sort(std::begin(A), std::begin(A)+4);
+
+	std::cout << "Depois:" << std::endl;
+	while(a != a+4)
+	{
+		std::cout << *a << " ";
+		a++;
+	}
+
+	//std::cout << "Ponteiro apontando para o primeiro elemento apos a rotação do vetor:" << std::endl << *a << std::endl;
+	return 0;
+
+}
